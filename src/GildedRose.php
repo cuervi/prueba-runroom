64332a5d846de05de97f3dc645af8cc5ab2f9ed3
Iván Cuerva <ivancuerva@gmail.com>
2021-07-12 15:37:47 +0200

    Reorganización del código del método updateQuality

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -15,49 +15,54 @@ class GildedRose
     function updateQuality() 
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
+            
+            //Cuando es "Sulfuras, Hand of Ragnaros no se modifica nada
+            if ($item->name == 'Sulfuras, Hand of Ragnaros') {
+                continue;
+            }
+
+            //Modificaciones que se aplican a 'Aged Brie' y 'Backstage passes to TAFKAL80ETC Concert'
+            if ($item->name == 'Aged Brie' or $item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                
                 if ($item->quality < 50) {
+                    
                     $item->quality = $item->quality + 1;
+                    
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                        
+                        if ($item->sell_in < 11 and $item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                        
+                        if ($item->sell_in < 6 and $item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
+                        
                     }
                 }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                
+                //Codigo que se ejecuta en ambos independientemente del valor de Quality
                 $item->sell_in = $item->sell_in - 1;
+                
+                if ($item->sell_in < 0 and $item->name == 'Aged Brie' and $item->quality < 50) {
+                    $item->quality = $item->quality + 1;
+                }
+                
+                if ($item->sell_in < 0 and $item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    $item->quality = $item->quality - $item->quality;
+                }  
+                
+                continue;
             }
-
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+            
+            //Codigo para el resto de opciones
+            $item->sell_in = $item->sell_in - 1;
+            
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
+                
+                if ($item->sell_in < 0 and $item->quality > 0) {
+                    $item->quality = $item->quality - 1;
                 }
             }
         }
